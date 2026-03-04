5eab5d4ab86bd9f2c42efe3d3e7adfdfc0f6521b
Ten cua em <email@example.com>
2026-03-05 01:23:53 +0700

    test(env): print env vars in main method before spring boot

--- StudyGroupProject_V2/Backend/src/main/java/com/lkl/studygroup/StudygroupApplication.java
+++ StudyGroupProject_V2/Backend/src/main/java/com/lkl/studygroup/StudygroupApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudygroupApplication {
 
 	public static void main(String[] args) {
+		System.out.println("\n\n====== SUPER EARLY ENVIRONMENT VARIABLES DUMP ======");
+		System.out.println("DB_POSTGRES_URL: " + System.getenv("DB_POSTGRES_URL"));
+		System.out.println("DB_USERNAME: " + System.getenv("DB_USERNAME"));
+		System.out.println("DB_MONGO_URI: " + System.getenv("DB_MONGO_URI"));
+		System.out.println("======================================================\n\n");
 		SpringApplication.run(StudygroupApplication.class, args);
 	}
 
